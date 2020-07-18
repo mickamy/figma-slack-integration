bfa1d1f2c0fea4af53fc55ccf6a0c3c1b30d260d
Tetsuro Mikami <t.mikami1992@gmail.com>
2020-07-19 01:23:49 +0900

    Add POST /file/comment endpoint

--- figma_slack_integration.rb
+++ figma_slack_integration.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'dotenv/load' if development?
+require 'json'
 
 Dir[File.join(__dir__, 'lib', '*.rb')].each do |file|
   require file
@@ -15,4 +16,11 @@ end
 
 get '/' do
   'Hi, this is Bannai Tarao.<br/>High collar is beautiful.'
+end
+
+post '/file/comment' do
+  body = request.body.read
+  hash = JSON.parse(body, symbolize_names: true)
+  response = FigmaResponse::FileComment.new(hash)
+  slack_client.post(response.markdown)
 end
